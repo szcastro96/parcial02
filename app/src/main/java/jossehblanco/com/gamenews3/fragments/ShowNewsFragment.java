4e1b8e395cb0efb8e21fe6535bbe3433edfc284b
jossehblanco <jossehblanco@gmail.com>
2018-06-16 04:55:25 -0600

    Modificaciones leves

--- app/src/main/java/jossehblanco/com/gamenews3/fragments/ShowNewsFragment.java
+++ app/src/main/java/jossehblanco/com/gamenews3/fragments/ShowNewsFragment.java
@@ -1,9 +1,9 @@
 package jossehblanco.com.gamenews3.fragments;
 
-import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
@@ -51,7 +51,7 @@ public class ShowNewsFragment extends Fragment {
         retrofitClient = new RetrofitClient();
         retro = retrofitClient.getClient("http://gamenewsuca.herokuapp.com/");
         serviceNews = retro.create(ServiceNews.class);
-        serviceNews.getNewsLoL("Bearer "+token.toString()).enqueue(new Callback<List<New>>() {
+        serviceNews.getNewsAll("Bearer "+token.toString()).enqueue(new Callback<List<New>>() {
             @Override
             public void onResponse(Call<List<New>> call, Response<List<New>> response) {
                 if(response.body()!= null){
